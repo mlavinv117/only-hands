1becfd676868a324bc2022489d6e3d9586645c38
Manuel Lavin <manuel.lavin.1989@gmail.com>
2022-12-02 10:11:11 +0400

    updated webcam

--- streamlit-app.py
+++ streamlit-app.py
@@ -41,7 +41,7 @@ with col4:
     #     #print(lmList)
     #     if len(lmList)==21:
     #         p.write(keypoints_preprocessor(lmList))
-    RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun1.l.google.com:19302"]}]})
+    RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun2.l.google.com:19302"]}]})
     webrtc_ctx = webrtc_streamer(
     key="WYH",
     mode=WebRtcMode.SENDRECV,
