c7f273e5f49626ad6a83a400e69faf8e7ca0ca55
Manuel Lavin <manuel.lavin.1989@gmail.com>
2022-12-02 09:30:08 +0400

    updated webcam

--- streamlit-app.py
+++ streamlit-app.py
@@ -43,10 +43,10 @@ with col4:
     #         p.write(keypoints_preprocessor(lmList))
     RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
     webrtc_ctx = webrtc_streamer(key="TEST",
-                                 #mode=WebRtcMode.SENDRECV,
+                                 mode=WebRtcMode.SENDRECV,
                                  rtc_configuration=RTC_CONFIGURATION,
-                                 #media_stream_constraints={"video": True, "audio": False},
-                                 #async_processing=True,
+                                 media_stream_constraints={"video": True, "audio": False},
+                                 async_processing=False,
                                  )
 
 #Right column to show prediction
