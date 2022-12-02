47f480e2bd89de621f97585217db38ade1287e07
Manuel Lavin <manuel.lavin.1989@gmail.com>
2022-12-02 11:39:18 +0400

    updated webcam

--- streamlit-app.py
+++ streamlit-app.py
@@ -21,23 +21,31 @@ with col3:
     st.write(' ')
 
 #Main body with 2 columns to split webcam and prediction
-col4, col5 = st.columns(2)
+col4, col5, col6 = st.columns(2)
 
 #Left column to show webcam
 
-RTC_CONFIGURATION = RTCConfiguration(
-    {
-        "iceServers": [{
-            "urls": ["turn:openrelay.metered.ca:80"],
-            "username": "openrelayproject",
-            "credential": "openrelayproject",
-            }]
-        }
-    )
-webrtc_ctx = webrtc_streamer(
-    key="WYH",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=handTracker,
-    async_processing=True,)
+with col4:
+    pass
+
+with col5:
+
+    RTC_CONFIGURATION = RTCConfiguration(
+        {
+            "iceServers": [{
+                "urls": ["turn:openrelay.metered.ca:80"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+                }]
+            }
+        )
+    webrtc_ctx = webrtc_streamer(
+        key="WYH",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        media_stream_constraints={"video": True, "audio": False},
+        video_processor_factory=handTracker,
+        async_processing=True,)
+
+with col6:
+    pass
