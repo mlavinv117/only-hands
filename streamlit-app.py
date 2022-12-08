0e837c3c96bede076fadfc602662113c7d351f46
Manuel Lavin <LVizcaino@slb.com>
2022-12-08 17:20:34 +0400

    update

--- streamlit-app.py
+++ streamlit-app.py
@@ -123,5 +123,9 @@ with tab2:
                machine learning methods.
 
                 The American Sign Language letter database of hand gestures represent a multi-class problem with 24 classes
-                of letters (excluding J and Z which require motion).
+                of letters (excluding J and Z which require motion). Each training and test case represents a label (0-25) as
+                a one-to-one map for each alphabetic letter A-Z (and no cases for 9=J or 25=Z because of gesture motions).
+                The training data (27,455 cases) and test data (7172 cases) are approximately half the size of the standard MNIST
+                but otherwise similar with a header row of label, pixel1,pixel2….pixel784 which represent a single 28x28 pixel image
+                with grayscale values between 0-255.
                """)
